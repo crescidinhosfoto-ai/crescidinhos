907c8ee78c6cc2d3d1ff9c543266263fdbbc1f3b
crescidinhosfoto-ai <crescidinhosfoto@gmail.com>
2026-05-17 17:03:05 -0300

    config.js

--- crescidinhos-final/src/config.js
+++ crescidinhos-final/src/config.js
@@ -8,7 +8,8 @@ export const PHOTOGRAPHER = {
   email: "crescidinhosfoto@gmail.com",
   name: "Crescidinhos Fotografia",
   owner: "Thais de Sa Nascimento Dutra",
-  cpf: "000.000.000-00", // ⚠️ Preencha seu CPF
+  cpf: "410.593.908-41", 
+  cnpj: "64.189.121/0001-06",
   phone: "(14) 99684-5521",
   whatsapp: "5514996845521",
   instagram: "@crescidinhosfotografia",
